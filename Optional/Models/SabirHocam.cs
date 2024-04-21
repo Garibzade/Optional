e8c3515481d162745224caffd64315009506c735
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-21 18:49:24 +0400

    Added OptionalTask

---
+++ Optional/Models/SabirHocam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional.Models
+{
+    internal class SabirHocam
+    {
+      public void Kesmek(Student student)
+        {
+            if (student.IsHelpToPlagiate || student.IsPlagiated) 
+            {
+                Console.WriteLine($"{student.Name}-ugurla kesildi ve tohmet verildi");
+            }
+            else { Console.WriteLine("ne kocurdurem nede kocurtmeye komek eliyirem:)"); }
+        }
+    }
+}
---
+++ Optional/Program.cs
@@ -0,0 +1,15 @@
+﻿using Optional.Models;
+
+namespace Optional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Student student=new Student("Garib",false,false);
+            SabirHocam sabirHocam = new SabirHocam();
+            sabirHocam.Kesmek(student);
+
+        }
+    }
+}
